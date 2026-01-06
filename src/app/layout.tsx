c99fa18400910503b95a772d543d6830a229dc67
naeimDz <naeimdz1703@gmail.com>
2026-01-06 16:21:31 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
